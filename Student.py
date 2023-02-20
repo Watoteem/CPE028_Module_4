6bd5ed27f1e10f0d26f9cbc8ca584f2815d8503d
Watoteem <qjdadvincula@tip.edu.ph>
2023-02-20 09:35:30 +0000

    Student.py

---
+++ Student.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+p1 = Student("Joshua Advincula", 22)
+p2 = Student("Miguel Reyes", 55)
+
+print("Name: ", p1.name)
+print("Age: ", p1.age)
+print("Name: ", p2.name)
+print("Age: ", p2.age)
