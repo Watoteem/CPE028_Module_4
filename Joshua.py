81412016ee9c99657fa39d5e20650215479953d7
Watoteem <qjdadvincula@tip.edu.ph>
2023-02-22 07:50:24 +0000

    Add Joshua.py

---
+++ Joshua.py
@@ -0,0 +1,11 @@
+class Joshua:
+    def __init__(self, age, name, sex):
+        self.age = age
+        self.name = name
+        self.sex = sex
+
+ad = Joshua(19,"Joshua Advincula","Male")
+
+print("Name: ", ad.name)
+print("Age: ", ad.age)
+print("Sex: ", ad.sex)
